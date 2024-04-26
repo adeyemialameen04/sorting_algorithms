092ec9d3c2d32a8df54930be94ddec9d0075267c
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-04-27 00:13:44 +0100

    fixed quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,11 +5,24 @@ void swap(int *x, int *y);
 void quicksort_recursion(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high);
 
+/**
+	* quick_sort - Sorts an array based on quick sort algo.
+* @array: The array to be sorted.
+* @size: The size of the array.
+* Return: Nothing.
+*/
 void quick_sort(int *array, size_t size)
 {
 	quicksort_recursion(array, 0, size - 1, size);
 }
 
+
+/**
+	* swap - Swap two integers.
+* @x: The first val.
+* @y: the second val.
+* Return: Nothing
+*/
 void swap(int *x, int *y)
 {
 	int tmp = *x;
@@ -17,34 +30,47 @@ void swap(int *x, int *y)
 	*y = tmp;
 }
 
+/**
+	* quicksort_recursion - calls the partition function recursively.
+* @array: The size of the array.
+* @low: The beginning of the array.
+* @high: The end of the array.
+* @size: The size of the array.
+* Return: Nothing.
+*/
 void quicksort_recursion(int *array, int low, int high, size_t size)
 {
 	if (low < high)
 	{
 		int pivot_index = partition(array, low, high);
+
 		quicksort_recursion(array, low, pivot_index - 1, size);
 		quicksort_recursion(array, pivot_index + 1, high, size);
 		print_array(array, size);
 	}
 }
 
+/**
+	* partition - Partitions the array.
+* @array: The array.
+* @low: The beginning of the array.
+* @high: The end of the array.
+* Return: The postion of the pivot.
+*/
 int partition(int *array, int low, int high)
 {
-	int pivot_value = array[low];
-	int i = low, j = high;
+	int pivot_value = array[high];
+	int i = low - 1, j;
 
-	while (i < j)
+	for (j = low; j <= high - 1; j++)
 	{
-		while (array[i] <= pivot_value && i <= high - 1)
+		if (array[j] < pivot_value)
+		{
 			i++;
-		
-		while (array[j] > pivot_value && j >= low + 1)
-			j--;
-
-		if (i < j)
 			swap(&array[i], &array[j]);
+		}
 	}
 
-	swap(&array[low], &array[j]);
-	return (j);
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
 }
